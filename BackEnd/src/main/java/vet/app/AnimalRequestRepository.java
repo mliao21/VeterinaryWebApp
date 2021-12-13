286ed19559baad5df8ea934949d2639920d537d3
mliao21 <83739501+mliao21@users.noreply.github.com>
2021-12-13 00:24:48 -0500

    fix user class bugs

--- BackEnd/src/main/java/vet/app/AnimalRequestRepository.java
+++
@@ -1,7 +0,0 @@
-//package vet.app;
-//
-//import org.springframework.data.jpa.repository.JpaRepository;
-//
-//public interface AnimalRequestRepository extends JpaRepository<AnimalRequests, Long>{
-//
-//}
